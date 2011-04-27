f7ccac9e608cb3545967e7e291009e56dd48c5d5
Chih-Wei Huang <cwhuang@linux.org.tw>
2011-04-27 15:11:32 +0800

    fix building issues on honeycomb
    The project external/clearsilver is removed in honeycomb.

--- Android.mk
+++ Android.mk
@@ -1,17 +1,5 @@
 LOCAL_PATH := $(call my-dir)
 
-# Make a static library for clearsilver's regex. This prevents multiple
-# symbol definition error.
-include $(CLEAR_VARS)
-LOCAL_SRC_FILES := ../clearsilver/util/regex/regex.c
-LOCAL_MODULE := libclearsilverregex
-LOCAL_MODULE_TAGS := optional
-LOCAL_C_INCLUDES := \
-	external/clearsilver \
-	external/clearsilver/util/regex
-include $(BUILD_STATIC_LIBRARY)
-
-
 SUBMAKE := make -s -C $(LOCAL_PATH) CC=$(CC)
 KERNEL_MODULES_DIR?=/system/lib/modules
 
@@ -33,10 +21,7 @@ endif
 BUSYBOX_C_INCLUDES = \
 	$(LOCAL_PATH)/include-$(BUSYBOX_CONFIG) \
 	$(LOCAL_PATH)/include $(LOCAL_PATH)/libbb \
-	external/clearsilver \
-	external/clearsilver/util/regex \
-	bionic/libc/private \
-	libc/kernel/common
+	bionic/libc/private
 
 BUSYBOX_CFLAGS = \
 	-std=gnu99 \
@@ -56,7 +41,6 @@ LOCAL_CFLAGS := $(BUSYBOX_CFLAGS)
 LOCAL_MODULE := busybox
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_PATH := $(TARGET_OUT_OPTIONAL_EXECUTABLES)
-LOCAL_STATIC_LIBRARIES += libclearsilverregex
 include $(BUILD_EXECUTABLE)
 
 BUSYBOX_LINKS := $(shell cat $(LOCAL_PATH)/busybox-$(BUSYBOX_CONFIG).links)
@@ -94,7 +78,7 @@ LOCAL_CFLAGS += \
   -Dgenerate_uuid=busybox_generate_uuid
 LOCAL_MODULE := libbusybox
 LOCAL_MODULE_TAGS := optional
-LOCAL_STATIC_LIBRARIES += libclearsilverregex libcutils libc libm 
+LOCAL_STATIC_LIBRARIES += libcutils libc libm
 include $(BUILD_STATIC_LIBRARY)
 
 # Build a static busybox for the recovery image
@@ -114,7 +98,7 @@ LOCAL_CFLAGS += \
 LOCAL_FORCE_STATIC_EXECUTABLE := true
 LOCAL_MODULE := utility_busybox
 LOCAL_MODULE_TAGS := eng
-LOCAL_STATIC_LIBRARIES += libclearsilverregex libcutils libc libm 
+LOCAL_STATIC_LIBRARIES += libcutils libc libm
 LOCAL_MODULE_CLASS := UTILITY_EXECUTABLES
 LOCAL_MODULE_PATH := $(PRODUCT_OUT)/utilities
 LOCAL_UNSTRIPPED_PATH := $(PRODUCT_OUT)/symbols/utilities
